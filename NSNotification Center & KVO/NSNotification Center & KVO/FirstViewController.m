a266b8a6895f1ba77e0d0d05c7301865a38cf995
Paul <paul.huynh3@gmail.com>
2017-09-16 18:13:57 -0400

    add dictionary to store the notification

--- NSNotification Center & KVO/NSNotification Center & KVO/FirstViewController.m
+++ NSNotification Center & KVO/NSNotification Center & KVO/FirstViewController.m
@@ -9,11 +9,36 @@
 #import "FirstViewController.h"
 
 @interface FirstViewController ()
+@property (weak, nonatomic) IBOutlet UIStepper *stepper;
+
+
 
 @end
 
 @implementation FirstViewController
 
+- (IBAction)stepperTpped:(UIStepper *)sender {
+    
+    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    
+    
+    NSDictionary *dictionary = [[NSDictionary alloc]initWithObjectsAndKeys: notificationCenter,@(self.stepper.value), nil];
+    
+    NSNotification *notification = [[NSNotification alloc]initWithName:@"countStep" object:self.stepper userInfo:dictionary];
+    
+
+    
+    
+    /* 
+Now post the notification using NSNotificationCenter's method - (void)postNotification:(NSNotification *)notification;. You can see that this is an instance method. It is called on the NSNotificationCenter's instance. You pass it the NSNotification you created.
+     
+     */
+-(void)postNotification:(NSNotification*)notification{
+    
+    
+    }
+    
+}
 
 
 
