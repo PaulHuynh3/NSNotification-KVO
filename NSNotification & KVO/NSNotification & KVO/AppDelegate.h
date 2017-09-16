05a970bd69f71fff31005901e992a228fa2e5dcf
Paul <paul.huynh3@gmail.com>
2017-09-16 16:33:13 -0400

    add tab bar

---
+++ NSNotification & KVO/NSNotification & KVO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSNotification & KVO
+//
+//  Created by Paul on 2017-09-16.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSNotification & KVO/NSNotification & KVO/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSNotification & KVO
+//
+//  Created by Paul on 2017-09-16.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
